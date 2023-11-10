ea7f1c1744caef892475e92e9d1327a31cff94a5
iTzoncE <joshuadanielchavez97@gmail.com>
2023-11-10 12:48:42 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -25,3 +25,11 @@ CREATE TABLE appointments (
     reason VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
+CREATE TABLE doctors(
+	doctorid INT PRIMARY KEY,
+	dfirstname VARCHAR(50) NOT NULL,
+	dlastname VARCHAR(50) NOT NULL,
+	specialty VARCHAR(100) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	email VARCHAR(100) NOT NULL
+);
